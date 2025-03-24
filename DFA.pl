661c8bc23cbf1a69dc394a1d92ec02798ee39235
Mateo Minghi <59071760+MateoMinghi@users.noreply.github.com>
2025-03-24 08:25:30 -0600

    minor details

--- DFA.pl
+++ DFA.pl
@@ -4,49 +4,41 @@
 % Must not contain "aa" or "ccb"
 
 % Transitions
-transition(s0, a, s1).
 transition(s0, b, s0).
-transition(s0, c, s2).
+transition(s0, a, s1).
+transition(s0, c, s4).
 
-transition(s1, a, s_dead).  
-transition(s1, b, s3).      
-transition(s1, c, s2).
+transition(s1, a, s1).
+transition(s1, b, s2).
+transition(s1, c, s4).
 
-transition(s2, b, s0).
-transition(s2, c, s5).
+transition(s2, a, s2).
+transition(s2, b, s2).
+transition(s2, c, s2).
 
-transition(s3, a, s4).  
+transition(s3, a, s3).
 transition(s3, b, s3).
-transition(s3, c, s6).
+transition(s3, c, s3).
 
-transition(s4, b, s3).
-transition(s4, c, s6).
+transition(s4, c, s4).
+transition(s4, a, s1).
+transition(s4, b, s5).
 
-transition(s5, c, s_dead).  
+transition(s5, a, s5).
+transition(s5, b, s5).
+transition(s5, c, s5).
 
-transition(s6, a, s4).
-transition(s6, b, s3).
-transition(s6, c, s7).
+% Final states
+final_state(s2).
 
-transition(s7, a, s4).
-transition(s7, c, s6).
-
-transition(s_dead, a, s_dead).
-transition(s_dead, b, s_dead).
-transition(s_dead, c, s_dead).
-
-final_state(s3).
-final_state(s4).
-final_state(s6).
-final_state(s7).
-
+% Check if a string is accepted by the DFA
 check_string(String) :-
     atom_chars(String, Chars),
     process_string(Chars, s0).
 
+% Process the string
 process_string([], CurrentState) :-
     final_state(CurrentState).
-
 process_string([Char|Rest], CurrentState) :-
     transition(CurrentState, Char, NextState),
     process_string(Rest, NextState).
